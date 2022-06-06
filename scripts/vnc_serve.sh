44c05a773c05d9f472a9fec9377303dc5aabbd5c
marchellodev <marchellodev@gmail.com>
2022-06-06 13:07:15 +0300

    vnc scripts & dash instead of bash

---
+++ scripts/vnc_serve.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# todo dislay tray icon notifiyng of the vnc server running
+
+wayvnc --render-cursor
+
--- upd.sh
+++ upd.sh
@@ -1,2 +1,9 @@
+#!/bin/sh
+
+# todo make sure all reqired software is installed
+
 git pull
 stow .
+
+sudo ln -sfT dash /usr/bin/sh
+
