826fe2dda7c6296a680ea5b40d43a808378adecf
marchellodev <marchellodev@gmail.com>
2022-07-13 22:33:41 +0300

    Ultrawide script upd

--- scripts/ultrawide.sh
+++ scripts/ultrawide.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-swaymsg -- output DP-1 pos 0 0 mode --custom 3440x1440@60Hz
+swaymsg -- output DP-1 pos 0 0 mode --custom 3440x1440@93Hz
 swaymsg -- output eDP-1 disable
 
 echo Success
